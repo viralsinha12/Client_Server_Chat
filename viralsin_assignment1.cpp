3f14288a0fb948d196872ac42f9cdb27da5eb4a3
viralsinha12 <viral.sinha@hotmail.com>
2019-02-24 17:34:35 -0500

    PORT command Implemented

--- viralsin_assignment1.cpp
+++ viralsin_assignment1.cpp
@@ -41,43 +41,79 @@ using namespace std;
  * @return 0 EXIT_SUCCESS
  */
 
-int printAuthor(char *);
-int getIP(char *);
+void printAuthor(char *);
+void getIP(char *);
+void commonPrintFunction(char *, char *);
+void getPortforServer(char *,char *);
+
 int main(int argc, char **argv)
 {
 	/*Init. Logger*/
 	cse4589_init_log(argv[2]);
+	char *tempChar = argv[1];
 	/* Clear LOGFILE*/
     fclose(fopen(LOGFILE, "w"));
 	/*Start Here*/
 	string cmd;
-	while(true){
-	cout << "#chat# : ";
-	cin >> cmd;
+	while(true)
+	{
+		cout << "\n#chat# : ";
+		cin >> cmd;
+		if(cmd == "AUTHOR")
+			printAuthor("AUTHOR");
 
-	if(cmd == "AUTHOR")
-		printAuthor("AUTHOR");
+		if(cmd == "EXIT")
+			break;
 
-	if(cmd == "EXIT")
-		break;
-
-	if(cmd == "IP")
-		getIP("IP");
+		if(cmd == "IP")
+			getIP("IP");
+		
+		if(cmd == "PORT")
+		{
+			commonPrintFunction("PORT",argv[2]);
+		}
 	}
 	return 0;
 }
 
-int getIP(char *cmd)
+// void getPortforServer(char *cmd, char *port)
+// {
+// 	int sockFd, addressStatus;
+// 	char ipstr[INET_ADDRSTRLEN];
+// 	struct  addrinfo hints;
+// 	struct  addrinfo *addressInfo;
+// 	memset(&hints,0,sizeof hints);
+// 	hints.ai_family = AF_INET;
+// 	hints.ai_socktype = SOCK_DGRAM;
+// 	hints.ai_flags = AI_PASSIVE;
+// 	if(addressStatus = getaddrinfo(NULL,port,&hints,&addressInfo)){
+// 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(addressStatus));
+// 	}
+
+// 	if((sockFd = socket(addressInfo->ai_family, addressInfo->ai_socktype, addressInfo->ai_protocol))== -1)
+// 	{
+// 		perror("listener: socket");
+// 	}
+
+// 	//bind(sockFd, addressInfo->ai_addr, addressInfo->ai_addrlen);
+// 	connect(sockFd,addressInfo->ai_addr,addressInfo->ai_addrlen);
+// 	struct sockaddr_in name ;
+// 	socklen_t namelen = sizeof(name);
+// 	getsockname(sockFd,(struct sockaddr *)&name,&namelen);	
+// 	cout<<ntohs(name.sin_port);
+// }
+
+void getIP(char *cmd)
 {
 	int sockFd, addressStatus;
-	char ipstr[INET6_ADDRSTRLEN];
+	char ipstr[INET_ADDRSTRLEN];
 	struct  addrinfo hints;
 	struct  addrinfo *addressInfo;
 	memset(&hints,0,sizeof hints);
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_flags = AI_PASSIVE;
-	if(addressStatus = getaddrinfo("8.8.8.8","53",&hints,&addressInfo)){
+	if(addressStatus = getaddrinfo("8.8.8.8","3453",&hints,&addressInfo)){
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(addressStatus));
 	}
 
@@ -85,25 +121,25 @@ int getIP(char *cmd)
 	{
 		perror("listener: socket");
 	}
-
 	connect(sockFd,addressInfo->ai_addr,addressInfo->ai_addrlen);
 	struct sockaddr_in name;
 	socklen_t namelen = sizeof(name);
 	getsockname(sockFd,(struct sockaddr *)&name,&namelen);
 	char buf[100];
 	inet_ntop(addressInfo->ai_family, &name.sin_addr, buf, sizeof buf);
-	cse4589_print_and_log("[%s:SUCCESS]\n",cmd);
-	cse4589_print_and_log("IP:%s\n",buf);
-	cse4589_print_and_log("[%s:END]\n",cmd);
-	return 0;
+	commonPrintFunction("IP",buf);
 }
 
-int printAuthor(char *authorName)
+void printAuthor(char *authorName)
 {
-	
-	char *myString = "I, Viral Sinha, have read and understood the course academic integrity policy.\n";
-	cse4589_print_and_log("[%s:SUCCESS]\n",authorName);
-	cse4589_print_and_log("AUTHOR:%s\n",myString);
-	cse4589_print_and_log("[%s:END]\n",authorName);
-	return 0;
+	char *myString = "I, Viral Sinha, have read and understood the course academic integrity policy.";
+	commonPrintFunction("AUTHOR",myString);
 }
+
+void commonPrintFunction(char *command, char *output)
+{
+	cse4589_print_and_log("[%s:SUCCESS]\n",command);
+	cse4589_print_and_log("%s:%s\n",command,output);
+	cse4589_print_and_log("[%s:END]\n",command);
+}
+
